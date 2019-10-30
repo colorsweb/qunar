e2a391411fcac544f97deb61f8467423c16cab59
colorsweb <42506623+colorsweb@users.noreply.github.com>
2019-10-30 14:10:02 +0800

    dist

---
+++ dist/precache-manifest.2561b402da4a5c5647f243200d0bd17f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d970aef9aefefd41376",
+    "url": "css/app.70fa9ccf.css"
+  },
+  {
+    "revision": "5d970aef9aefefd41376",
+    "url": "js/app.538ebeed.js"
+  },
+  {
+    "revision": "c75588504ed4ae33dc2d",
+    "url": "css/chunk-vendors.4062b2be.css"
+  },
+  {
+    "revision": "c75588504ed4ae33dc2d",
+    "url": "js/chunk-vendors.e91d2f0c.js"
+  },
+  {
+    "revision": "4e347924b4313ba58452787c1595b9f3",
+    "url": "img/image.4e347924.svg"
+  },
+  {
+    "revision": "7c6926eeb7251c0416ff9c7c20a9cfa6",
+    "url": "img/back.7c6926ee.svg"
+  },
+  {
+    "revision": "340a5dad74b64911ef0ba0d996e1a957",
+    "url": "img/star.340a5dad.svg"
+  },
+  {
+    "revision": "eabad8b33e75835bd46ce059b27d9fdc",
+    "url": "index.html"
+  },
+  {
+    "revision": "325c39a846c0cce2db621400f676143d",
+    "url": "mock/city.json"
+  },
+  {
+    "revision": "52e5a2b71653b92df288bd065b5754b2",
+    "url": "mock/detail.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "cb3cf10f989c00100ccc5834daf443cf",
+    "url": "mock/index.json"
+  }
+];
